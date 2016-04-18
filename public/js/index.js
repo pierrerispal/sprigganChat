168afe9009e8e0f3000df9e610a90e49c3efc20a
Pierre <pierre.rispal@gmail.com>
2016-04-18 10:33:54 +0200

    clean initial

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,11 @@
 var socket = io(),
     newMessagesCount = 0,
     channel='';
-    chatTitle = "Chat Paleur";
+    chatTitle = "Spriggan chat";
 
 $('#login form').submit(function () {
     if($('#pseudo').val().trim()!="" && $('#channel').val().trim()!=""){
-        $('#login').css('display','none');
+        /*$('#login').css('display','none');
         $('#chat').css('display','block');
         var pseudo = $('#pseudo').val().trim();
         channel = $('#channel').val().trim();
@@ -14,78 +14,78 @@ $('#login form').submit(function () {
             pseudo:pseudo,
             channel:channel
         };
-        socket.emit('connect user',user);
+        socket.emit('connect user',user);*/
     }
     return false;
 });
 
 $('#chat form').submit(function(){
-    var msg = $('#m').val(),
+    /*var msg = $('#m').val(),
         send = {
             channel:channel,
             msg:msg.trim()
         };
     socket.emit('chat message', send);
     $('#m').val('');
-    return false;
+    return false;*/
 });
 
 $('#chat').focusin(function () {
-    clearTitle();
+    //clearTitle();
 });
 $('#chat').click(function () {
-    clearTitle();
+    //clearTitle();
 })
 
-$('#expand').click(function(){
+/*$('#expand').click(function(){
     $('#list').toggleClass("open");
-});
+});*/
 
 socket.on('connect user', function(user){
-    if(user.channel==channel){
+    /*if(user.channel==channel){
         var msg = user.pseudo+" est connecté.";
         $('#messages').append($('<li class="connection">').text(msg));
         scrollChatDown();
-    }
+    }*/
 });
 
 socket.on('disconnect user', function(user){
-    if(user.channel==channel){
+    /*if(user.channel==channel){
         var msg = user.pseudo+" est déconnecté.";
         $('#messages').append($('<li class="unconnection">').text(msg));
         scrollChatDown();
-    }
+    }*/
 });
 
-socket.on('clear list', function(){
+/*socket.on('clear list', function(){
     $('#list #userList').text('');
-});
+});*/
 
-socket.on('user list', function(user){
+/*socket.on('user list', function(user){
     if(user.channel==channel){
         $('#list #userList').append($('<li class="list-group-item">').text(user.pseudo));
     }
-});
+});*/
 
 socket.on('chat message', function(send){
-    if(send.channel==channel){
+    /*if(send.channel==channel){
         var msg = send.pseudo+" : "+send.msg;
         $('#messages').append($('<li>').html(msg));
         //Add 1 to the "lastMessages" and show it in the doc title
         newMessagesCount++;
         $(document).prop('title', '('+newMessagesCount+") - "+chatTitle);
         scrollChatDown();
-    }
+    }*/
 });
 
-socket.on('clear title', function(){
+/*socket.on('clear title', function(){
     clearTitle();
-});
+});*/
 
-function clearTitle(){
+/*function clearTitle(){
     newMessagesCount=0;
     $(document).prop('title',chatTitle);
-}
+}*/
 
 function scrollChatDown(){
     var messageHeight = $('#messages').height();
